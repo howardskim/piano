71141dca1ff05b4accbbd223d5e548b4c919b34e
Howard Kim <howardkim820@gmail.com>
2020-03-11 14:57:17 -0700

    finished front end

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const keys = document.querySelectorAll('.key');
+const blackKeys = ['s', 'd', 'g', 'h', 'j'];
+const whiteKeys = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const blackDivs = document.querySelectorAll('.black');
+const whiteDivs = document.querySelectorAll('.white')
 keys.forEach((key) => {
     key.addEventListener('click', () => {
         playAudio(key);
@@ -6,12 +10,25 @@ keys.forEach((key) => {
 });
 
 function playAudio(keyDiv){
-    console.log('key ', keyDiv);
     keyDiv.classList.toggle('active')
     let note = keyDiv.getAttribute('data-note');
-    console.log(note)
     let audioToPlay = document.getElementById(note);
-    console.log(audioToPlay)
     audioToPlay.currentTime = 0;
     audioToPlay.play();
+    audioToPlay.addEventListener('ended', () => {
+        keyDiv.classList.remove('active')
+    })
 }
+document.addEventListener('keydown', (e) => {
+   if(e.repeat) return;
+   let keyPressed = e.key;
+   let whiteIndex = whiteKeys.indexOf(keyPressed);
+   let blackIndex = blackKeys.indexOf(keyPressed)
+   if(whiteIndex > -1){
+        playAudio(whiteDivs[whiteIndex])
+   } else if (blackIndex > -1){
+       playAudio(blackDivs[blackIndex])
+   } else {
+       return;
+   }
+})
